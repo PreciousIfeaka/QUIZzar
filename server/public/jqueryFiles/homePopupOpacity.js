ba6ea2d641040362bf7ca7af97189842950e4589
PreciousIfeaka <preciousifeaka@gmail.com>
2024-05-29 11:03:57 +0100

    designed the login, register and home page

---
+++ server/public/jqueryFiles/homePopupOpacity.js
@@ -0,0 +1,27 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.quiz-popup');
+const exitQuiz = document.querySelector('.exit-quiz');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const topicsContainer = document.querySelector('.container');
+const topicsBack = document.querySelector('.topics-back');
+
+startBtn.onclick = () => {
+  popupInfo.classList.add('active');
+  main.classList.add('active');
+}
+
+exitQuiz.onclick = () => {
+  popupInfo.classList.remove('active');
+  main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+  topicsContainer.classList.add('active');
+  popupInfo.classList.remove('active');
+  main.classList.remove('active');
+}
+
+topicsBack.onclick = () => {
+  topicsContainer.classList.remove('active');
+}
