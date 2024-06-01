aee1d89deea29c5c3683ca5c237583cf08f3b148
PreciousIfeaka <preciousifeaka@gmail.com>
2024-06-01 11:45:04 +0100

    added html and style for quiz page

--- server/public/jqueryFiles/homePopupOpacity.js
+++ server/public/jqueryFiles/homePopupOpacity.js
@@ -5,6 +5,8 @@ const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const topicsContainer = document.querySelector('.container');
 const topicsBack = document.querySelector('.topics-back');
+// const categoryBtns = document.querySelectorAll('.category-btn');
+
 
 startBtn.onclick = () => {
   popupInfo.classList.add('active');
@@ -25,3 +27,13 @@ continueBtn.onclick = () => {
 topicsBack.onclick = () => {
   topicsContainer.classList.remove('active');
 }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   categoryBtns.forEach(button => {
+//     button.addEventListener('click', (i) => {
+//       const selectedCategory = i.target.getAttribute('data-category');
+
+//       localStorage.setItem('selectedCategory', selectedCategory);
+//     });
+//   });
+// });
---
+++ server/public/jqueryFiles/questionToggle.js
@@ -0,0 +1,25 @@
+import generateQuestions from "../../model/questionsModel.js";
+
+const nextBtn = document.querySelector(".next-btn");
+const questionText = document.querySelector(".question-text");
+
+let questionCount = 0;
+
+const amount = 20;
+const category = 9;
+const difficulty = 'easy';
+const url =`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
+
+nextBtn.onclick = async () => {
+  questionText.classList.add('active');
+  questionCount++;
+  const questions = await generateQuestions(url);
+  console.log(questions);
+  if (questions.length > 0) {
+    showQuestion(questionCount, questions);
+  } else {console.log('No available questions')}
+}
+
+ async function showQuestion (index, questions) {
+  questionText.textContent = `${questions[index].number}. ${questions[index].question}`;
+}
