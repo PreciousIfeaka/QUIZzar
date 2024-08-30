d27ca0bbede7d61ec0ec9afd7391ed9627c259ce
PreciousIfeaka <preciousifeaka@gmail.com>
2024-08-30 06:35:12 +0100

    refactor: reorgnized repository architecture

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,10 +1,16 @@
 const pino  = require("pino");
 const dayjs = require("dayjs");
+require("dotenv").config();
+
+const transport = process.env.NODE_ENV !== 'production' ? {
+  target: "pino-pretty",
+  options: {
+    colorize: true
+  }
+} : undefined;
 
 const log = pino({
-  transport: {
-    target: "pino-pretty",
-  },
+  transport,
   base: {
     pid: false,
   },
